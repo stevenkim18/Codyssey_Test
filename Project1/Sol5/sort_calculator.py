5fe8488e42bab6a44c450e99399efa590b66eb25
stevenkim18 <11882@naver.com>
2025-07-14 13:55:52 +0900

    Project1 Sol5

---
+++ Project1/Sol5/sort_calculator.py
@@ -0,0 +1,37 @@
+def get_numbers():
+    user_input = input("여러개의 실수를 공백으로 구분하여 입력: ")
+    str_numbers = user_input.split()
+
+    if not str_numbers:
+        raise ValueError("Invalid input.")
+
+    numbers = []
+    for s in str_numbers:
+        try:
+            num = float(s)
+            numbers.append(num)
+        except ValueError:
+            raise ValueError("Invalid input.")
+    
+    return numbers
+
+def bubble_sort(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+
+def main():
+    try:
+        numbers = get_numbers()
+    except ValueError as e:
+        print(e)
+        return 
+        
+    sorted_numbers = bubble_sort(numbers)
+    print("Sorted:", sorted_numbers)
+
+if __name__ == "__main__":
+    main()
